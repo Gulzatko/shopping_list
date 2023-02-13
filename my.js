e6893cd75f5ccb18133d7d68ca1d134674555ca7
Gulzatko <Mamajanovna86@gmail.com>
2023-02-13 16:39:09 +0400

    adding local storage

--- my.js
+++ my.js
@@ -2,35 +2,76 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+const alert = document.querySelector(".alert")
 
 //functions
 const addTodo=(event)=>{
     //prevent button fromsubmitting the fucntion
     event.preventDefault()
+     if(!todoInput.value) {
+        alert.innerText = "Please enter a valid value"
+        alert.style.visibility = 'visible'
+        alert.classList.add("error")
+        setTimeout(()=>{
+        alert.style.visibility = 'hidden'
+        alert.classList.remove("error")
+       },1500)
+     } else {
+        const todoDiv= document.createElement("div")
+        todoDiv.classList.add("todoDiv")
+        todoDiv.innerHTML = `
+            <li class="todo-item">${todoInput.value}</li>
+            <button class="complete-btn">
+                <i class="fas fa-check"></i> 
+             </button>
+             <button class="delete-btn">
+                <i class="fas fa-trash"></i>
+             </button>`
+          todoList.appendChild(todoDiv)
+          saveLocalTodos(todoInput.value)
+       // clearing the input after pressing the button 
+           todoInput.value=""
+           alert.innerText= "item Successfully added"
+           alert.style.visibility = 'visible'
+           alert.classList.add("success")
+           setTimeout(()=>{
+            alert.style.visibility = 'hidden'
+            alert.classList.remove("success")
+
+           },1500)
+         }
+       }
     //creating tododiv 
-    const todoDiv= document.createElement("div")
-    todoDiv.classList.add("todoDiv")
-  // creatye LI
-  const newTodo = document.createElement("li")
-  newTodo.classList.add("todo-item")
-  newTodo.innerText= todoInput.value
-  todoDiv.appendChild(newTodo)
+   
+    
+    /*
+     <div class="todoDiv">
+        <li class="todo-item"></li>
+        <button class="complete-btn"></button>
+        <button class="delete-btn"></button>
+     </div>
+
+     */
+  // create LI
+  // const newTodo = document.createElement("li")
+  // newTodo.classList.add("todo-item")
+  // newTodo.innerText= todoInput.value
+  // todoDiv.appendChild(newTodo)
+
   // creating completed button 
-  const completedButton = document.createElement("button")
-  completedButton.classList.add("completed-btn")
-  completedButton.innerHTML = '<i class="fas fa-check"></i>'
-  todoDiv.appendChild(completedButton)
-  todoList.appendChild(todoDiv)
+  // const completedButton = document.createElement("button")
+  // completedButton.classList.add("completed-btn")
+  // completedButton.innerHTML = '<i class="fas fa-check"></i>'
+  // todoDiv.appendChild(completedButton)
+  // todoList.appendChild(todoDiv)
   // creating  deleted button 
-  const deleteButton = document.createElement("button")
-  deleteButton.classList.add("delete-btn")
-  deleteButton.innerHTML = '<i class="fas fa-trash"></i>'
-  todoDiv.appendChild(deleteButton)
+  // const deleteButton = document.createElement("button")
+  // deleteButton.classList.add("delete-btn")
+  // deleteButton.innerHTML = '<i class="fas fa-trash"></i>'
+  // todoDiv.appendChild(deleteButton)
   // append todoDiv to UL 
-  todoList.appendChild(todoDiv)
-  // clearing the input after pressing the button 
-  todoInput.value=""
-}
+ 
 
   const deleteComplete = (event) => {
   const item = event.target
@@ -41,13 +82,75 @@ const addTodo=(event)=>{
        todo.remove()
      })
      
-  } else if (item.classList.contains("completed-btn")){
+  } else if (item.classList.contains("complete-btn")){
     const todo = item.parentElement;
     todo.classList.toggle("completed");
    
    }
 }
 
-//event-listeners 
-todoButton.addEventListener("click", addTodo);
+const filterTodo = (event)=>{
+  const todos = todoList.childNodes
+  todos.forEach(function(todo){
+     console.log(event.target.value)
+     switch(event.target.value) {
+          case "all":
+          todo.style.display = "flex"
+          break;
+          case "completed":
+          if(todo.classList.contains("completed")){
+             todo.style.display= "flex"
+          } else {
+            todo.style.display = "none"
+          }
+          break;
+          case "uncompleted":
+            if(!todo.classList.contains("completed")){
+              todo.style.display = "flex"
+           } else {
+             todo.style.display = "none"
+           }
+           break;
+      }
+  })
+}
+
+const saveLocalTodos = (todo)=>{
+  let todos;
+  if(localStorage.getItem('todos')== null){
+    todos = []
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"))
+  }
+  todos.push(todo)
+  localStorage.setItem("todos",JSON.stringify(todos))
+}
+
+const getData =()=> {
+  let todos;
+  if(localStorage.getItem('todos')== null){
+    todos = []
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"))
+  }
+   todos.forEach(function(todo){
+
+    const todoDiv= document.createElement("div")
+    todoDiv.classList.add("todoDiv")
+    todoDiv.innerHTML = `
+        <li class="todo-item">${todo}</li>
+        <button class="complete-btn">
+            <i class="fas fa-check"></i> 
+         </button>
+         <button class="delete-btn">
+            <i class="fas fa-trash"></i>
+         </button>`
+      todoList.appendChild(todoDiv)
+   })
+}
+
+  //event-listeners 
+todoButton.addEventListener("click", addTodo)
 todoList.addEventListener("click",deleteComplete)
+filterOption.addEventListener("click", filterTodo)
+document.addEventListener("DOMContentLoaded",getData)
